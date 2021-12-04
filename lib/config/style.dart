bd9cf737485c96a72476fc30e676aad9260a69c1
hasyimzii <arihasyim.zii@gmail.com>
2021-12-04 19:08:47 +0700

    Add main & style

---
+++ lib/config/style.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+Color whiteColor = const Color(0xFFFFFFFF);
+Color blackColor = const Color(0xFF2C3E50);
+Color lightColor = const Color(0xFFECF0F1);
+Color greyColor = const Color(0xFFCACACA);
+Color blueColor = const Color(0xFF2980B9);
+Color greenColor = const Color(0xFF33CCCC);
+
+TextStyle titleTextStyle =  TextStyle(
+  color: blackColor,
+  fontWeight: FontWeight.w600,
+  fontSize: 20,
+);
+
+TextStyle subtitleTextStyle = TextStyle(
+  color: greyColor,
+  fontSize: 14,
+);
+
+TextStyle titleListTextStyle = TextStyle(
+  color: blackColor,
+  fontWeight: FontWeight.w500,
+  fontSize: 15,
+);
+
+TextStyle subtitleListTextStyle = TextStyle(
+  color: greyColor,
+  fontWeight: FontWeight.w300,
+  fontSize: 13,
+);
+
+TextStyle infoTextStyle = TextStyle(
+  color: blackColor,
+  fontWeight: FontWeight.w500,
+  fontSize: 13,
+);
