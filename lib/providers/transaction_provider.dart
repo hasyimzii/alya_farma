45d968a8e34e2b9fd935576f83d4b9697352fe56
hasyimzii <arihasyim.zii@gmail.com>
2022-01-25 00:46:28 +0700

    Add transaction model & provider

--- lib/providers/transaction_provider.dart
+++ lib/providers/transaction_provider.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/cupertino.dart';
 import '../models/tansaction.dart';
+import '../models/cart.dart';
+import '../models/medic.dart';
 
 class TransactionProvider with ChangeNotifier {
   final List<Transaction> _transaction = [];
 
-  List<Transaction> get medic => _transaction;
+  List<Transaction> get transaction => _transaction;
   int get length => _transaction.length;
+
+  void addTransaction(Medic medic, int amount) {
+    return;
+  }
 }
