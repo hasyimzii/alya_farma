73bb3c94d8b8a82526faa110cd3f5e902438527f
hasyimzii <arihasyim.zii@gmail.com>
2022-01-25 00:45:33 +0700

    Update transaction history

--- lib/views/transaction_history.dart
+++ lib/views/transaction_history.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import '../widgets/transaction_content.dart';
+import '../widgets/history_content.dart';
 
 class TransactionHistory extends StatelessWidget {
   const TransactionHistory({Key? key}) : super(key: key);
@@ -11,7 +11,7 @@ class TransactionHistory extends StatelessWidget {
     return ListView.builder(
       itemCount: 10,
       itemBuilder: (BuildContext context, int index) {
-        return TransactionContent(
+        return HistoryContent(
           image: 'https://images.tokopedia.net/img/cache/500-square/VqbcmM/2020/10/3/eefb843f-53fa-4345-b1bc-946f15a42072.jpg',
           name: 'produk',
           price: 10000,
---
+++ lib/widgets/history_content.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+import '../common/style.dart';
+import '../common/rupiah.dart';
+
+class HistoryContent extends StatelessWidget {
+  final String image;
+  final String name;
+  final int price;
+  final int amount;
+  final Object onTapArgs;
+  final VoidCallback onBuy;
+
+  const HistoryContent({
+    Key? key,
+    required this.image,
+    required this.name,
+    required this.price,
+    required this.amount,
+    required this.onTapArgs,
+    required this.onBuy,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final int totalPrice = price * amount;
+
+    return Container(
+      margin: const EdgeInsets.symmetric(
+        horizontal: 15,
+        vertical: 10,
+      ),
+      decoration: BoxDecoration(
+        color: whiteColor,
+        borderRadius: BorderRadius.circular(10),
+        boxShadow: boxShadow(3),
+      ),
+      child: Column(
+        children: [
+          ListTile(
+            leading: ClipRRect(
+              borderRadius: BorderRadius.circular(10.0),
+              child: AspectRatio(
+                aspectRatio: 1.0 / 1.0,
+                child: Image.network(
+                  image,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            title: Text(
+              name,
+              style: titleText(15),
+              overflow: TextOverflow.ellipsis,
+              maxLines: 1,
+            ),
+            subtitle: Text(
+              Rupiah.convert(price),
+              style: priceText(13),
+            ),
+            trailing: Text(
+              '${amount}x',
+              style: lightText(13),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              vertical: 5,
+              horizontal: 10,
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  children: [
+                    Text(
+                      'Total Belanja',
+                      style: lightText(10),
+                    ),
+                    Text(
+                      Rupiah.convert(totalPrice),
+                      style: titleText(12),
+                    ),
+                  ],
+                ),
+                ElevatedButton(
+                  child: Text(
+                    'Beli Lagi',
+                    style: whiteText(10),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    fixedSize: const Size(75, 8),
+                    shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(10),
+                      ),
+                    ),
+                    primary: greenColor,
+                  ),
+                  onPressed: onBuy,
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
